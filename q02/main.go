437b5469618d909b90a8f5f0651274ff548a270c
Seth Norman <san72@cornell.edu>
2024-11-15 10:24:05 -0600

    Quest 2, Part II

--- q02/main.go
+++ q02/main.go
@@ -13,17 +13,37 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	r := regexp.MustCompile("WORDS:(.*)\n\n(.*)\n")
+	r := regexp.MustCompile("(?s)^WORDS:(.*)\n\n(.*)\n$")
 	matches := r.FindSubmatch(input)
 	words := bytes.Split(matches[1], []byte{','})
-	count := 0
+	wordMap := make(map[string]bool)
 	for _, word := range words {
-		text := matches[2]
-		i := bytes.Index(text, word)
-		for i >= 0 {
-			count += 1
-			text = text[i+len(word):]
-			i = bytes.Index(text, word)
+		rev := make([]byte, len(word))
+		for i, c := range word {
+			rev[len(rev)-1-i] = c
+		}
+		wordMap[string(word)] = true
+		wordMap[string(rev)] = true
+	}
+
+	lines := bytes.Split(matches[2], []byte{'\n'})
+
+	count := 0
+	for _, line := range lines {
+		next := 0
+		for i := range line {
+			for word := range wordMap {
+				if i+len(word) > len(line) {
+					continue
+				}
+				poss := line[i : i+len(word)]
+				if word == string(poss) {
+					overlap := max(0, next-i)
+					count += max(0, len(word)-overlap)
+
+					next = max(next, i+len(word))
+				}
+			}
 		}
 	}
 	fmt.Println(count)
