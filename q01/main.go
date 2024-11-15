2a1e1c63b708679539bb5c73d3866c8d877732ab
Seth Norman <san72@cornell.edu>
2024-11-14 19:55:30 -0600

    Quest 1, Part II

--- q01/main.go
+++ q01/main.go
@@ -11,18 +11,38 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	input = input[:len(input)-1]
+	if len(input)%2 != 0 {
+		log.Fatal("input has odd number of enemies")
+	}
+
 	var potions int
-	for _, c := range input {
-		switch c {
-		case 'A':
-		case 'B':
-			potions += 1
-		case 'C':
-			potions += 3
-		case '\n':
-		default:
-			log.Fatalf("invalid enemy: '%s'", string(c))
-		}
+	i := 0
+	for i < len(input) {
+		potions += calculatePotionsNeededForPair(input[i], input[i+1])
+		i += 2
 	}
 	fmt.Println(potions)
+}
+
+func calculatePotionsNeededForPair(a, b byte) int {
+	return max(0, 2+calculatePotionsNeededForEnemy(a)+calculatePotionsNeededForEnemy(b))
+}
+
+func calculatePotionsNeededForEnemy(c byte) int {
+	switch c {
+	case 'A':
+		return 0
+	case 'B':
+		return 1
+	case 'C':
+		return 3
+	case 'D':
+		return 5
+	case 'x':
+		return -2
+	default:
+		log.Fatalf("invalid enemy: '%s'", string(c))
+	}
+	panic("unreachable")
 }
