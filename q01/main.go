de14f9502c70528bff5a75db55c6b7b5ce4b6897
Seth Norman <san72@cornell.edu>
2024-11-14 20:15:11 -0600

    Quest 1, Part III

--- q01/main.go
+++ q01/main.go
@@ -12,35 +12,47 @@ func main() {
 		log.Fatal(err)
 	}
 	input = input[:len(input)-1]
-	if len(input)%2 != 0 {
+	if len(input)%3 != 0 {
 		log.Fatal("input has odd number of enemies")
 	}
 
 	var potions int
 	i := 0
 	for i < len(input) {
-		potions += calculatePotionsNeededForPair(input[i], input[i+1])
-		i += 2
+		potions += calculatePotionsNeededForBattle(input[i : i+3])
+		i += 3
 	}
 	fmt.Println(potions)
 }
 
-func calculatePotionsNeededForPair(a, b byte) int {
-	return max(0, 2+calculatePotionsNeededForEnemy(a)+calculatePotionsNeededForEnemy(b))
+func calculatePotionsNeededForBattle(enemies []byte) int {
+	numEnemies := len(enemies)
+	potions := 0
+	for _, e := range enemies {
+		p, ok := calculatePotionsNeededForEnemy(e)
+		if !ok {
+			numEnemies -= 1
+			continue
+		}
+		potions += p
+	}
+	potions += numEnemies * (numEnemies - 1)
+
+	return max(0, potions)
 }
 
-func calculatePotionsNeededForEnemy(c byte) int {
+func calculatePotionsNeededForEnemy(c byte) (int, bool) {
 	switch c {
 	case 'A':
-		return 0
+		return 0, true
 	case 'B':
-		return 1
+		return 1, true
 	case 'C':
-		return 3
+		return 3, true
 	case 'D':
-		return 5
+		return 5, true
 	case 'x':
-		return -2
+		return 0, false
 	default:
 		log.Fatalf("invalid enemy: '%s'", string(c))
 	}
